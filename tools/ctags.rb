c0daf3eeecf13faa01f23ae6af6a2bd1c53194dc
unknown <kozlopio@WW2003647.eu.tieto.com>
2013-05-24 07:28:06 +0200

    script for tagging

---
+++ tools/ctags.rb
@@ -0,0 +1,7 @@
+includes = ['C_Application/SC_OAM/BBC_BasebandBusConfiguration/src', 'C_Application/SC_OAM/DM_FSM/src', 'C_Application/SC_OAM/FoundationModel/src', 'C_Application/SC_OAM/InternalInterfaces/src', 'C_Application/SC_OAM/include', 'I_Interface', 'C_Test/SC_OAM/MT_COMMONS']
+ctagsFileName = "ctags_includes"
+File.open(ctagsFileName, 'w') do |ff|
+    includes.each do |include|
+        ff.puts Dir.pwd.gsub(/C_Test.*/,'') + include
+    end
+end
