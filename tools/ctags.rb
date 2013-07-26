42c7f8317ebb9e6b0c6b6eb46f00ac27bae3edbe
unknown <kozlopio@WW2003647.eu.tieto.com>
2013-07-26 13:02:10 +0200

    ycm generator

--- tools/ctags.rb
+++ tools/ctags.rb
@@ -1,4 +1,4 @@
-includes = ['C_Application/SC_OAM/BBC_BasebandBusConfiguration/src', 'C_Application/SC_OAM/DM_FSM/src', 'C_Application/SC_OAM/FoundationModel/src', 'C_Application/SC_OAM/InternalInterfaces/src', 'C_Application/SC_OAM/include', 'I_Interface', 'C_Test/SC_OAM/MT_COMMONS', 'C_Application/SC_OAM/InternalInterfaces/include']
+includes = ['C_Application/SC_OAM/BBC_BasebandBusConfiguration/src', 'C_Application/SC_OAM/DM_ALMAG/src', 'C_Application/SC_OAM/DM_FSM/src', 'C_Application/SC_OAM/FoundationModel/src', 'C_Application/SC_OAM/InternalInterfaces/src', 'C_Application/SC_OAM/include', 'I_Interface', 'C_Test/SC_OAM/MT_COMMONS', 'C_Application/SC_OAM/InternalInterfaces/include']
 ctagsFileName = "ctags_includes"
 File.open(ctagsFileName, 'w') do |ff|
     includes.each do |include|
